66fd78c9470b31e5a0d15a069d4083a4cb583bc6
Will Thomas <wlthomas001@gmail.com>
2016-09-11 14:01:01 -0600

    add train data

--- api/meetup.js
+++ api/meetup.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 module.exports = {
     getMeetup: function() {
         return new Promise((resolve, reject) => {
-                var url = 'https://api.meetup.com/2/events/?radius=50.0&order=time&group_urlname=denver-tech&offset=0&format=json&page=20&fields=&key='+ process.env.MEETUP_KEY+'&desc=false&status=upcoming'
+                var url = 'https://api.meetup.com/self/events?photo-host=public&page=20&key=461e18115b2357727591361195d3f50'
 
             function callback(error, response, body) {
                 if (!error && response.statusCode == 200) {
