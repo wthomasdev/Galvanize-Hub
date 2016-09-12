896e244ef82478307cc221e9e25efdcd454e10e8
Will Thomas <wlthomas001@gmail.com>
2016-09-12 10:27:04 -0600

    change meetup response to rsvp'd events

--- api/meetup.js
+++ api/meetup.js
@@ -4,10 +4,11 @@ require("dotenv").config();
 module.exports = {
     getMeetup: function() {
         return new Promise((resolve, reject) => {
-                var url = 'https://api.meetup.com/self/events?photo-host=public&page=20&key=461e18115b2357727591361195d3f50'
+                var url = 'https://api.meetup.com/self/events?&sign=true&photo-host=public&scroll=next_upcoming&page=20&key=' + process.env.MEETUP_KEY;
 
             function callback(error, response, body) {
                 if (!error && response.statusCode == 200) {
+                  console.log("Got to here")
                     resolve(body)
                     };
                 }
