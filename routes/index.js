ad70f6142a5c7fd093c6c194d1835d2f10739ff0
Will Thomas <wlthomas001@gmail.com>
2016-09-11 18:28:17 -0600

    setting up train data get request

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var api = require('../api/meetup');
+var train = require('../api/rtd');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,5 +9,9 @@ router.get('/', function(req, res, next) {
     res.send(data);
   })
 });
+
+router.get('/trains', (req, res, next) => {
+  res.send(train);
+})
 
 module.exports = router;
